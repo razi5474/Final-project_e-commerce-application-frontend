72aef659724598b840a93240a3c447f0a13b0d73
Razil kp <rasrasil69@gmail.com>
2025-07-10 00:17:43 +0530

    added a file

--- src/components/layout/UserNavbar.jsx
+++ src/components/layout/UserNavbar.jsx
@@ -24,7 +24,7 @@ const UserNavbar = () => {
     }
     finally{
       dispatch(clearUser());
-      navigate("/login"); // Redirect to login page after logout
+      navigate("/login", { replace: true });// Redirect to login page after logout
     }
 
   };
